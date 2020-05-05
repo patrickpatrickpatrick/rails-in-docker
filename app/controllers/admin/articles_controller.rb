c007538270bd26c18502952192a55196177789a8
Angela Wolff <whatapalaver@angelasvandapro.lan>
2020-05-05 17:57:27 +0100

    Added ability to add refs to article on create and edit

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -13,17 +13,18 @@ class Admin::ArticlesController < ApplicationController
 
   def new
     @article = Article.new
+    5.times { @article.item_references.build }
   end
 
   def edit
     @item_referenceable = @article
     @item_references = @item_referenceable.item_references
     @item_reference = ItemReference.new
+    5.times { @article.item_references.build }
   end
 
   def create
     @article = Article.new(article_params)
-
     if @article.save
       redirect_to admin_article_path(@article), notice: 'Article was successfully created.'
     else
@@ -52,6 +53,10 @@ class Admin::ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :content)
+      params.require(:article).permit(:title,
+                                      :content,
+                                      item_references_attributes: [:item_unique_id,
+                                                                   :id, :item_reference_id,
+                                                                   :item_reference_type])
     end
 end
--- app/models/item_reference.rb
+++ app/models/item_reference.rb
@@ -1,3 +1,7 @@
 class ItemReference < ApplicationRecord
   belongs_to :item_referenceable, polymorphic: true
+
+  validates_uniqueness_of :item_unique_id,  scope: [:item_referenceable_type, :item_referenceable_id],
+                           message: 'cannot be duplicated'
+  
 end
