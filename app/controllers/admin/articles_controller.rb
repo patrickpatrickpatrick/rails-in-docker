cd1127e7d86729e8caed3f95a03005923147df6e
Angela Wolff <whatapalaver@angelasvandapro.lan>
2020-05-05 11:04:43 +0100

    Set up namespaced Articles and Events

---
+++ app/controllers/admin/articles_controller.rb
@@ -0,0 +1,51 @@
+class Admin::ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to admin_article_path(@article), notice: 'Article was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to admin_article_path(@article), notice: 'Article was successfully updated.' 
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to admin_articles_url, notice: 'Article was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def article_params
+      params.require(:article).permit(:title, :content)
+    end
+end
